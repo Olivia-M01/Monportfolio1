359970606bcdc80550ac7aaf71c541318ab8f1cd
Olivia-M01 <olivia_mavakala22@icloud.com>
2025-05-30 12:45:24 +0200

    modifications

---
+++ mapage.js
@@ -0,0 +1,12 @@
+<sript>
+    const toggleBtn= document.getElemenytById('menu-btn');
+    const menu-lat= document.getElemenytById('menu-lat');
+    const overlay= document.getElemenytById('overlay';)
+
+    toggleBtn.addEventListener('click',() {
+        menu-lat.classList.toggle('show');
+        overlay.classList.toggle('show');
+    });
+
+    overlay.addEventListener('click', ()  )
+</sript>
